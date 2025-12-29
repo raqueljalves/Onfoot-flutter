30e1adf20c4cd6c47682d25daea8a2a8ca97e36d
Raquel Alves <raquelalvesptb@gmail.com>
2025-12-29 13:06:31 +0100

    retoques

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
 
     defaultConfig {
         applicationId = "com.onfoot.app"
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
